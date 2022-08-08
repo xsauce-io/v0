68487c75c45906d7a2f2b31d95b17a258bc3d4f3
Zach Miller <home@mac-2.local>
2022-08-08 19:15:54 -0400

    again

--- components/feed.jsx
+++ components/feed.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import "react-responsive-carousel/lib/styles/carousel.min.css"
 export const Feed = () => {
 
 return (
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,13 +12,13 @@ import {
   createClient,
   WagmiConfig,
 } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
+// import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider } = configureChains(
   [chain.mainnet, chain.polygon, chain.optimism, chain.polygonMumbai],
   [
-    alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
+    // alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
     publicProvider()
   ]
 );
