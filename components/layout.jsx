e1c7612e8c3f4a9deab0237407913a9e7a7f6dc4
Jessie  Detchou <jessie.detchou@gmail.com>
2022-09-03 00:35:13 -0400

    layout

--- components/layout.jsx
+++ components/layout.jsx
@@ -3,6 +3,8 @@ import { Footer } from './footer';
 import { Tabs } from './tabs';
 import { Announcement } from './announcement';
 import { Countdown } from './countdown';
+import { BsArrowRight } from 'react-icons/bs'
+import { HiOutlineBookOpen } from 'react-icons/hi'
 
 export const Layout = ({ children }) => {
     return (
@@ -16,32 +18,39 @@ export const Layout = ({ children }) => {
                 <Nav />
 
                 {/* //TODO: Export the div as Hero component */}
-                <div class='w-full  py-10 mt-[2rem]'>
+                <div class='w-[100%]  py-10 mt-[2rem]'>
                     <grid class="grid grid-cols-3">
                         <div class="col-span-1">
                             <h1 class='laptop:text-5xl py-10'>Xchange</h1>
                         </div>
                         <div class="col-span-2 flex justify-end">
-                            <div class="flex flex-col h-full w-1/2  justify-end items-end space-y-4">
+                            <div class="flex flex-col h-full w-3/7  space-y-4">
 
-                                <button class='bg-[#DCDEE1] rounded-[10px] p-4 w-[100%] hover:opacity-40'>
-                                    Learn how the Xchange works
+                                <button class='bg-[#DCDEE1] rounded-[10px] p-4 px-8 w-[100%] hover:opacity-40 flex '>
+                                    <HiOutlineBookOpen class='self-center ' />
+                                    <p class="px-4 ">Learn how the Xchange works</p>
+                                    <BsArrowRight class='self-center ' />
                                 </button>
 
-                                <div class='bg-[#DCDEE1] rounded-[10px] p-4 text-left w-[100%]'>
+                                <div class='bg-[#DCDEE1] rounded-[10px] p-4 text-left w-[100%] '>
                                     <h1 class="py-2" >FINANCIAL OVERVIEW</h1>
                                     <grid class="grid grid-cols-3">
                                         <div class="grid grid-rows-2">
-                                            <div class="text-[grey]">Positions</div>
+                                            <div class="text-[grey] flex ">
+                                                <p class="inline-block mr-1">Positions</p>
+                                                <BsArrowRight class='self-center' />
+                                            </div>
                                             <div>12 positions</div>
                                         </div>
                                         <div class="grid grid-rows-2">
                                             <div class="text-[grey]">Xsauce tokens</div>
                                             <div>3,702 $SAUX</div>
                                         </div>
-                                        <div class="grid grid-rows-2">
+                                        <div class="grid grid-rows-2 ">
                                             <div></div>
-                                            <div ><button class="rounded-2xl px-3 bg-[#ACFF00] "> redeem</button></div>
+                                            <div class=' justify-self-center' >
+                                                <button class="rounded-2xl px-3 bg-[#ACFF00] "> redeem</button>
+                                            </div>
                                         </div>
 
                                     </grid>
