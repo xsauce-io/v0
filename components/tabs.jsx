a700670779e05e2d04216bd791655ed6190eb1c0
Jessie  Detchou <jessie.detchou@gmail.com>
2022-09-02 23:59:32 -0400

    active-tabs

--- components/tabs.jsx
+++ components/tabs.jsx
@@ -10,21 +10,21 @@ export const Tabs = (props) => {
         <div className="w-full items-center justify-center">
             <div class="tabs w-[100%] border-black border-b-2">
                 <Link href="/">
-                    <a class={router.pathname == "/" ? "tab tab-bordered tab-active transition" : "tab tab-bordered  transition"}>Dashboard</a>
+                    <a class={router.pathname == "/" ? "tab border-[#ACFF00] border-b-[2px] transition text-black" : "tab transition"}>Dashboard</a>
                 </Link>
                 <Link href="/livemarkets">
-                    <a class={router.pathname == "/livemarkets" ? "tab tab-bordered tab-active transition" : "tab tab-bordered  transition"}>Live Market</a>
+                    <a class={router.pathname == "/livemarkets" ? "tab  border-b-[2px] border-[#ACFF00]  transition text-black " : "tab transition"}>Live Market</a>
                 </Link>
                 <Link href="/premarkets">
-                    <a class={router.pathname == "/premarkets" ? "tab tab-bordered tab-active transition" : "tab tab-bordered  transition"}>Pre Market</a>
+                    <a class={router.pathname == "/premarkets" ? "tab border-[#ACFF00] border-b-[2px] transition text-black" : "tab transition"}>Pre Market</a>
                 </Link>
                 <Link href="/redeem">
-                    <a class={router.pathname == "/redeem" ? "tab tab-bordered tab-active transition" : "tab tab-bordered  transition"}>Cash Out</a>
+                    <a class={router.pathname == "/redeem" ? "tab  border-[#ACFF00] border-b-[2px] transition text-black" : "tab transition"}>Cash Out</a>
                 </Link>
             </div>
             <div class="py-10">
                 {props.children}
             </div>
-        </div >
+        </div>
     )
 }
