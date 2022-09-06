555d7f826065bfbe98e7e68e97380912a3555c20
Jessie  Detchou <jessie.detchou@gmail.com>
2022-09-05 22:29:40 -0400

    heroku test

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 import { useRouter } from "next/router"
 import React from "react"
 import { BsTelegram } from "react-icons/bs"
-import { FaGithub, FaRegHandPeace, FaTwitter, FaTwitterSquare } from "react-icons/Fa"
+import { FaGithub, FaRegHandPeace, FaTwitter, FaTwitterSquare } from "react-icons/fa"
 
 export const Footer = ({ children }) => {
     return (
