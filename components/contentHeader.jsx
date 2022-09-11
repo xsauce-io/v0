3f3f1c3e04bec6667e819df9cefee1e1ead953d7
Jessie  Detchou <jessie.detchou@gmail.com>
2022-09-10 21:43:46 -0400

    responsive header

--- components/contentHeader.jsx
+++ components/contentHeader.jsx
@@ -5,12 +5,12 @@ export const ContentHeader = ({ title, children, icon }) => {
 
     return (
         <div className='w-[100%] text-inherit py-10 '>
-            <div className="flex flex-col laptop:flex-row space-x-4 items-center">
+            <div className="flex flex-col tablet:flex-row space-x-4 items-center">
                 <div className="flex justify-start space-x-3" >
                     {icon}
-                    <h3 className='text-3xl font-SG'>{title}</h3>
+                    <p className='text-2xl tablet:text-3xl  font-SG'>{title}</p>
                 </div>
-                <div className="flex justify-start">
+                <div className="flex justify-start mobile:mt-4  tablet:mt-0 flex-0">
                     {children}
                 </div>
             </div>
