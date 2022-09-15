dc2f641e8fef21ac780650c10f54b26f03221bb9
Jessie  Detchou <jessie.detchou@gmail.com>
2022-09-14 22:08:44 -0400

    deprecated premarket

--- components/tabs.jsx
+++ components/tabs.jsx
@@ -17,9 +17,9 @@ export const Tabs = ({ children, bgColor }) => {
                     <Link href="/livemarkets">
                         <a className={router.pathname == "/livemarkets" ? "tab border-b-[3px] border-[#ACFF00] transition text-black px-0 mobile:text-xs tablet:text-sm" : "tab transition text-[#748282] px-0 mobile:text-xs tablet:text-sm"} style={router.pathname == "/livemarkets" ? { color: 'inherit' } : null}>Live Market</a>
                     </Link>
-                    <Link href="/premarkets">
+                    {/* <Link href="/premarkets">
                         <a className={router.pathname == "/premarkets" ? "tab border-[#ACFF00] border-b-[3px] transition text-black px-0 mobile:text-xs tablet:text-sm" : "tab transition text-[#748282] px-0 mobile:text-xs tablet:text-sm"} style={router.pathname == "/premarkets" ? { color: 'inherit' } : null}>Pre Market</a>
-                    </Link>
+                    </Link> */}
                     <Link href="/redeem">
                         <a className={router.pathname == "/redeem" ? "tab border-[#ACFF00] border-b-[3px] transition text-black px-0 mobile:text-xs tablet:text-sm" : "tab transition text-[#748282] px-0 mobile:text-xs tablet:text-sm"} style={router.pathname == "/redeem" ? { color: 'inherit' } : null}>Cash Out</a>
                     </Link>
