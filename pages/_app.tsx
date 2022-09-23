52766bd00316299d6e25818bb124642a9ae7d753
Jessie  Detchou <jessie.detchou@gmail.com>
2022-09-23 19:32:30 -0400

    changed toast notification package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,14 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { ToastProvider} from 'react-toast-notifications';
-
-
-
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import toast, { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-
-    <ToastProvider>
-        <Component {...pageProps} />
-        </ToastProvider>
-     
-  );
+	return (
+		<>
+			<Component {...pageProps} />
+			<Toaster />
+		</>
+	);
 }
 
 export default MyApp;
