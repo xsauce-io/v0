42c5a67c127066e2e35e09aaf3d91c5710b36258
Jessie  Detchou <jessie.detchou@gmail.com>
2022-09-24 21:09:29 -0400

    setup data analytics -test

--- components/announcement.jsx
+++ components/announcement.jsx
@@ -1,13 +1,17 @@
-import React from "react"
+import React from 'react';
 
 export const Announcement = () => {
-
-  return (
-    <div className=" p-2 text-inherit bg-inherit font-SG flex flex-row justify-center items-center mobile:text-sm laptop:text-[12px] w-full">
-      <p> * We are in Phase Zero. All wagers are in paper money until we officialy launch.</p>&nbsp;
-
-      <a className="underline hover:text-inherit" href="docs.xsauce.io"> Learn more</a>&nbsp;*
-    </div>
-
-  )
-}
+	return (
+		<div className=" p-2 text-inherit bg-inherit font-SG flex flex-row justify-center items-center mobile:text-sm laptop:text-[12px] w-full">
+			<p>
+				* We are in Phase Zero. All wagers are in paper money until we officialy
+				launch.
+			</p>
+			&nbsp;
+			<a className="underline hover:text-inherit" href="docs.xsauce.io">
+				Learn more
+			</a>
+			&nbsp;*
+		</div>
+	);
+};
