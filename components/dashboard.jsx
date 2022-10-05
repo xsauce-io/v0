2b7df0bf4f9b47642dff6e77e53fb037b553c911
Jessie  Detchou <jessie.detchou@gmail.com>
2022-10-05 16:47:59 -0400

    dashboard into grid

--- components/dashboard.jsx
+++ components/dashboard.jsx
@@ -14,62 +14,34 @@ export const Dashboard = ({ positions }) => {
 		desktop: '1400',
 	};
 
-
-  let truncateContract = positions?.address.substring(0, 4) + '...' + positions?.address.slice(-4);
+	let truncateContract =
+		positions?.address.substring(0, 4) + '...' + positions?.address.slice(-4);
 
 	const explorer = 'https://goerli.etherscan.io/address/' + positions?.address;
 
-	
 	const { width } = useWindowDimensions();
 	return (
 		<a>
-			{width >= screens.desktop ? (
-				<div className="flex flex-row  text-[12px] rounded-[40px] w-full bg-[#0C1615] text-white font-Inter items-center justify-between py-4 px-8 my-[.5rem]">
-					<span className="flex flex-row  w-[30%] font-SG text-sm">
+			{width >= screens.tablet ? (
+				<div className="flex flex-row grid grid-cols-9 text-[12px] rounded-[40px] w-full bg-[#0C1615] text-white font-Inter items-center justify-between py-4 px-8 my-[.5rem]">
+					<span className="flex flex-row col-span-3 font-SG text-sm ">
 						{positions?.name}
 					</span>
-					<span className="flex flex-row  w-[22%] text-xs font-Inter justify-center pl-4">
+					<span className="flex flex-row col-span-2 text-xs font-Inter ">
 						{positions?.amountYes} Yes / {positions?.amountNo} No
 					</span>
 
-					<span className="flex flex-row w-[38%] text-xs justify-center pr-3">
+					<span className="flex flex-row col-span-3 text-xs ">
 						<span className="bg-[#ACFF00] text-black rounded-[40px] py-1 px-2 flex ">
 							<p className="text-xs font-Inter">
-								{' '}
-								{positions?.avgBuyPriceYes} ¢ (yes) /{' '}
-								{positions?.avgBuyPriceNo} ¢ (no){' '}
-							</p>
-						</span>
-					</span>
-
-					<a
-						href={explorer}
-						className="flex flex-row w-[10%] text-xs font-Inter underline"
-					>
-						{truncateContract} ↗
-					</a>
-				</div>
-			) : width >= screens.tablet ? (
-				<div className="flex flex-row  text-[12px] rounded-[40px] w-full bg-[#0C1615] text-white font-Inter items-center justify-between py-4 px-8 my-[.5rem]">
-					<span className="flex flex-row  w-[35%] font-SG text-sm ">
-          {positions?.name}
-					</span>
-					<span className="flex flex-row  w-[25%] text-xs font-Inter justify-center">
-						{positions?.amountYes} Yes / {positions?.amountNo} No
-					</span>
-
-					<span className="flex flex-row w-[25%] text-xs justify-center">
-						<span className="bg-[#ACFF00] text-black rounded-[40px] py-1 px-2 flex ">
-							<p className="text-xs font-Inter">
-								{' '}
-								{positions?.avgBuyPriceYes} ¢ (yes) /{' '}
-								{positions?.avgBuyPriceNo} ¢ (no)
+								{positions?.avgBuyPriceYes} ¢ (yes) / {positions?.avgBuyPriceNo}{' '}
+								¢ (no)
 							</p>
 						</span>
 					</span>
 					<a
 						href={explorer}
-						className="flex flex-row w-[25%] text-xs font-Inter underline justify-center"
+						className="flex flex-row col-span-1 text-xs font-Inter underline"
 					>
 						{truncateContract} ↗
 					</a>
@@ -77,12 +49,12 @@ export const Dashboard = ({ positions }) => {
 			) : (
 				<a
 					href={explorer}
-					className="flex flex-row  text-[12px] rounded-[40px] w-full bg-[#0C1615] text-white font-Inter items-center justify-between py-4 px-8 my-[.5rem]"
+					className="flex flex-row grid grid-cols-2 text-[12px] rounded-[40px]  space-x-2 w-full bg-[#0C1615] text-white font-Inter items-center justify-between py-4 px-8 my-[.5rem]"
 				>
-					<span className="flex flex-row  w-[40%] font-SG text-xs text-ellipsis overflow-hidden ">
-          {positions?.name}
+					<span className="flex flex-row col-span-1 font-SG text-xs text-ellipsis overflow-hidden ">
+						{positions?.name}
 					</span>
-					<span className="flex flex-row  w-[50%] text-xs font-Inter justify-center bg-[#ACFF00] text-black rounded-[40px] py-1 px-2">
+					<span className="flex flex-row col-span-1 text-xs font-Inter justify-center bg-[#ACFF00] text-black rounded-[40px] py-1 ">
 						{positions?.amountYes} Yes / {positions?.amountNo} No
 					</span>
 				</a>
--- components/dashboardTable.jsx
+++ components/dashboardTable.jsx
@@ -1,108 +1,55 @@
-
-import React from "react"
-import { useWindowDimensions } from '/utils/hooks/useWindowDimensions.js'
+import React from 'react';
+import { useWindowDimensions } from '/utils/hooks/useWindowDimensions.js';
 
 export const DashboardTable = ({ children }) => {
-    const screens = {
-        mobile: "300",
-        tablet: "640",
-        smlaptop: "1024",
-        laptop: "1200",
-        desktop: "1400"
-    }
+	const screens = {
+		mobile: '300',
+		tablet: '640',
+		smlaptop: '1024',
+		laptop: '1200',
+		desktop: '1400',
+	};
 
-    const { width } = useWindowDimensions();
+	const { width } = useWindowDimensions();
 
-    return (
+	return (
+		<div className="flex flex-col w-full ">
+			{width >= screens.tablet ? (
+				<div className=" grid grid-cols-9 py-4 px-8 text-[14px] items-start w-full  font-Inter ">
+					<div className="flex flex-row col-span-3   space-x-2 items-center ">
+						<p>Name of Assets</p>
+						<img src="/up-down.svg" />
+					</div>
 
-        <div className="flex flex-col w-full ">
-            {width >= screens.desktop ? (
-                <div className="flex flex-row py-4 text-[14px]  items-center w-full px-4 font-Inter">
-                    <div className="flex flex-row pl-4 w-[40%] space-x-2 items-center ">
-                        <p >Name of Assets</p>
-                        <img src="/up-down.svg" />
-                    </div>
+					<div className="flex flex-row col-span-2  space-x-2 items-center">
+						<p>Holdings</p>
+						<img src="/up-down.svg" />
+					</div>
 
-                    <div className="flex flex-row w-[25%] space-x-2 items-center">
-                        <p  >Holdings</p>
-                        <img src="/up-down.svg" />
-                    </div>
+					<div className="flex flex-row col-span-3  space-x-2 items-center">
+						<p>Avg Buy Price</p>
+						<img src="/up-down.svg" />
+					</div>
 
-                    <div className="flex flex-row w-[25%] space-x-2 items-center">
-                        <p >Avg Buy Price</p>
-                        <img src="/up-down.svg" />
-                    </div>
+					<div className="flex flex-row col-span-1  space-x-2 items-center">
+						<p>Contract</p>
+						<img src="/up-down.svg" />
+					</div>
+				</div>
+			) : (
+				<div className="grid grid-cols-2 py-4 text-[12px] px-4  space-x-2 items-start w-full font-Inter">
+					<div className="flex flex-row col-span-1 space-x-2 items-center ">
+						<p>Name of Assets</p>
+						<img src="/up-down.svg" />
+					</div>
 
-                    <div className="flex flex-row w-[11%]  space-x-2 items-center">
-                        <p>Contract</p>
-                        <img src="/up-down.svg" />
-                    </div>
-                </div>
-            ) : width >= screens.laptop ? (
-                <div className="flex flex-row py-4 text-[14px] font-Inter items-center w-full px-4">
-                    <div className="flex flex-row pl-4 w-[25%] space-x-2 items-center w-full flex-1 ">
-                        <p>Name of Asset</p>
-                        <img src="/up-down.svg" />
-                    </div>
-
-                    <div className="flex flex-row w-[25%] space-x-2 items-center justify-center">
-                        <p>Holdings</p>
-                        <img src="/up-down.svg" />
-                    </div>
-
-                    <div className="flex flex-row w-[25%] space-x-2 items-center justify-center">
-                        <p>Average Buy Price</p>
-                        <img src="/up-down.svg" />
-                    </div>
-
-                    <div className="flex flex-row w-[25%]  space-x-2 items-center justify-center">
-                        <p>Contract</p>
-                        <img src="/up-down.svg" />
-                    </div>
-                </div>
-            ) : width >= screens.tablet ? (
-                <div className="flex flex-row py-4 text-[14px] font-Inter items-center w-full px-4">
-                    <div className="flex flex-row pl-4 w-[35%] space-x-2 items-center w-full flex-1 ">
-                        <p>Name of Asset</p>
-                        <img src="/up-down.svg" />
-                    </div>
-
-                    <div className="flex flex-row w-[27%] space-x-2 items-center justify-center">
-                        <p>Holdings</p>
-                        <img src="/up-down.svg" />
-                    </div>
-
-                    <div className="flex flex-row w-[25%] space-x-2 items-center">
-                        <p>Avg Buy Price</p>
-                        <img src="/up-down.svg" />
-                    </div>
-
-                    <div className="flex flex-row w-[17%]  space-x-2 items-center">
-                        <p>Contract</p>
-                        <img src="/up-down.svg" />
-                    </div>
-                </div>
-            ) : (
-                <div className="flex flex-row py-4 text-[14px] font-Inter items-center w-full px-4 space-x-2">
-                    <div className="flex flex-row pl-4 w-[40%] items-center ">
-                        <p>Name of Asset</p>
-                        <img src="/up-down.svg" />
-                    </div>
-
-                    <div className="flex flex-row w-[28.5%] items-center">
-                        <p>Holdings</p>
-                        <img src="/up-down.svg" />
-                    </div>
-
-                    <div className="flex flex-row w-[32%] items-center ">
-                        <p>Contract</p>
-                        <img src="/up-down.svg" />
-                    </div>
-                </div>
-            )}
-            {children}
-        </div>
-
-
-    )
-}
+					<div className="flex flex-row col-span-1 space-x-2 items-center">
+						<p>Avg Buy Price</p>
+						<img src="/up-down.svg" />
+					</div>
+				</div>
+			)}
+			{children}
+		</div>
+	);
+};
