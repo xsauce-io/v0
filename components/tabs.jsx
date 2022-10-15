f90005014128829e253177fe9f666b3a45095d01
Jessie  Detchou <jessie.detchou@gmail.com>
2022-10-15 17:59:16 -0400

    modified tabs

--- components/tabs.jsx
+++ components/tabs.jsx
@@ -1,33 +1,86 @@
-import { useRouter } from "next/router";
-import React from "react";
+import { useRouter } from 'next/router';
+import React from 'react';
 import Link from 'next/link';
 
 export const Tabs = ({ children, bgColor }) => {
-    const router = useRouter();
+	const router = useRouter();
 
-
-    return (
-        <div className="w-full items-center justify-center border-0" >
-            <div className="px-5 laptop:px-40 font-Inter border-0" style={{ backgroundColor: bgColor }
-            }>
-                <div className="tabs border-[#000] border-b-[1px] border-t-[0px] w-full text-white space-x-3 tablet:space-x-8 " >
-                    <Link href="/">
-                        <a className={router.pathname == "/" ? "tab border-[#ACFF00] border-b-[3px] transition px-0 mobile:text-xs tablet:text-sm " : "tab transition mobile:text-xs tablet:text-sm text-[#748282] px-0 "} style={router.pathname == "/" ? { color: 'inherit' } : null}>Dashboard</a>
-                    </Link>
-                    <Link href="/calendar">
-                        <a className={router.pathname == "/calendar" ? "tab border-[#ACFF00] border-b-[3px] transition px-0 mobile:text-xs tablet:text-sm " : "tab transition mobile:text-xs tablet:text-sm text-[#748282] px-0 "} style={router.pathname == "/calendar" ? { color: 'inherit' } : null}>Calendar</a>
-                    </Link>
-                    <Link href="/livemarkets">
-                        <a className={router.pathname == "/livemarkets" ? "tab border-b-[3px] border-[#ACFF00] transition text-black px-0 mobile:text-xs tablet:text-sm" : "tab transition text-[#748282] px-0 mobile:text-xs tablet:text-sm"} style={router.pathname == "/livemarkets" ? { color: 'inherit' } : null}>Live Market</a>
-                    </Link>
-                    <Link href="/redeem">
-                        <a className={router.pathname == "/redeem" ? "tab border-[#ACFF00] border-b-[3px] transition text-black px-0 mobile:text-xs tablet:text-sm" : "tab transition text-[#748282] px-0 mobile:text-xs tablet:text-sm"} style={router.pathname == "/redeem" ? { color: 'inherit' } : null}>Cash Out</a>
-                    </Link>
-                </div>
-            </div>
-            <div className="pb-10">
-                {children}
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="w-full items-center justify-center border-0">
+			<div
+				className="px-5 laptop:px-40 font-Inter border-0 "
+				style={{ backgroundColor: bgColor }}
+			>
+				<div className=" border-[#000] border-b-[1px] border-t-[0px] w-full text-white space-x-3 tablet:space-x-8 flex flex-nowrap ">
+					<Link href="/">
+						<a
+							className={
+								router.pathname == '/'
+									? 'flex flex-row items-center text-center border-[#ACFF00] border-b-[3px] transition px-0 mobile:text-xs tablet:text-sm  pb-1'
+									: 'flex flex-row items-center text-center transition mobile:text-xs tablet:text-sm text-[#748282] px-0  pb-1'
+							}
+							style={router.pathname == '/' ? { color: 'inherit' } : null}
+						>
+							Home
+						</a>
+					</Link>
+					<Link href="/calendar">
+						<a
+							className={
+								router.pathname == '/calendar'
+									? 'flex flex-row items-center text-center border-[#ACFF00] border-b-[3px] transition px-0 mobile:text-xs tablet:text-sm  pb-1'
+									: 'flex flex-row items-center text-center transition mobile:text-xs tablet:text-sm text-[#748282] px-0 pb-1'
+							}
+							style={
+								router.pathname == '/calendar' ? { color: 'inherit' } : null
+							}
+						>
+							Calendar
+						</a>
+					</Link>
+					<Link href="/livemarkets">
+						<a
+							className={
+								router.pathname == '/livemarkets'
+									? 'flex flex-row items-center text-center border-b-[3px] border-[#ACFF00] transition text-black px-0 mobile:text-xs tablet:text-sm pb-1'
+									: 'flex flex-row items-center  text-center transition text-[#748282] px-0 mobile:text-xs tablet:text-sm pb-1'
+							}
+							style={
+								router.pathname == '/livemarkets' ? { color: 'inherit' } : null
+							}
+						>
+							Live Market
+						</a>
+					</Link>
+					<Link href="/redeem">
+						<a
+							className={
+								router.pathname == '/redeem'
+									? ' flex flex-row items-center text-center border-[#ACFF00] border-b-[3px] transition text-black px-0 mobile:text-xs tablet:text-sm pb-1'
+									: 'flex flex-row items-center text-center transition text-[#748282] px-0 mobile:text-xs tablet:text-sm pb-1'
+							}
+							style={router.pathname == '/redeem' ? { color: 'inherit' } : null}
+						>
+							Cash Out
+						</a>
+					</Link>
+					<Link href="/portfolio">
+						<a
+							className={
+								router.pathname == '/portfolio'
+									? 'flex flex-row items-center text-center justify-center border-[#ACFF00] border-b-[3px] transition text-black px-0 mobile:text-xs tablet:text-sm pb-1'
+									: 'flex flex-row items-center text-center transition text-[#748282] px-0 mobile:text-xs tablet:text-sm pb-1'
+							}
+							style={
+								router.pathname == '/portfolio' ? { color: 'inherit' } : null
+							}
+						>
+							Portfolio
+						</a>
+					</Link>
+				</div>
+			</div>
+			<div className="pb-10">{children}</div>
+		</div>
+	);
+};
---
+++ pages/portfolio.tsx
@@ -0,0 +1,176 @@
+import type { NextPage } from 'next';
+import { Layout } from '../components/layout';
+import Head from 'next/head';
+import { useState, useEffect, useMemo } from 'react';
+import axios from 'axios';
+import React from 'react';
+import { ethers } from 'ethers';
+import { TopStories } from '../components/topStories';
+import { DashboardTable } from '../components/dashboardTable';
+import { MarketFactory, marketsDataGit } from '../services/constants';
+import MarketFactoryABI from '../abi/marketFactory.json';
+import MarketAbi from '../abi/markets.json';
+import { CalendarHighlight } from '../components/calendarHighlight';
+import { useGetMarketBySku, useGetSneaker } from '../services/useRequests';
+
+// Here we have used react-icons package for the icons
+// And react-slick as our Carousel Lib
+
+import { Dashboard } from '../components/dashboard';
+import { ContentHeader } from '../components/contentHeader';
+import { Skeleton } from '@mui/material';
+import toast from 'react-hot-toast';
+import { ToastNotification } from '../components/toast';
+declare let window: any;
+
+const Portfolio: NextPage = () => {
+	// ------------------- Constants ---------------------
+	const screens = {
+		mobile: '300',
+		tablet: '640',
+		smlaptop: '1024',
+		laptop: '1200',
+		desktop: '1400',
+	};
+	const SORT_BY_STATES = {
+		RELEASE_DATE: 'releaseDate',
+		NAME: 'name',
+		RETAIL_PRICE: 'retailPrice',
+	};
+
+	const skeletonArray = [1, 2, 3, 4, 5, 6, 7, 8];
+
+	// ------------------- State Variable --------------------
+
+	const [response, setResponse] = useState([] as any);
+	const [storedPersistentResponse, setStoredPersistentResponse] = useState(
+		[] as any[]
+	);
+
+	//filter state mana
+	const [isAscending, setIsAscending] = useState(true);
+	const [sortBy, setSortBy] = useState({ state: SORT_BY_STATES.RELEASE_DATE });
+
+	// -------------------- Data Fetching ------------------
+
+	const [allBalances, setAllBalances] = useState([] as any);
+
+	const showBalances = async () => {
+		const hasConnectedWalletBefore = localStorage.getItem(
+			'hasConnectedWalletBefore'
+		);
+
+		if (hasConnectedWalletBefore != null) {
+			const provider = new ethers.providers.Web3Provider(window.ethereum);
+			const connected = (
+				await provider.send('eth_requestAccounts', [0])
+			).toString();
+			const signer = provider.getSigner();
+			const contract2 = new ethers.Contract(
+				MarketFactory,
+				MarketFactoryABI,
+				signer
+			);
+			const allMarkets = await contract2.getAllMarketswSku();
+
+			const cleanedAllMarkets: any[] = [];
+			for (let index = 0; index < allMarkets.length; index++) {
+				const r1 = allMarkets[index].sku.toString();
+				const r2 = allMarkets[index].market.toString();
+				const r3 = allMarkets[index].name.toString();
+				const newData = { sku: r1, address: r2, name: r3 };
+				cleanedAllMarkets.push(newData);
+			}
+
+			const balanceArray: any = [];
+
+			for (let index = 0; index < cleanedAllMarkets?.length; index++) {
+				const contract = new ethers.Contract(
+					cleanedAllMarkets[index]?.address,
+					MarketAbi,
+					signer
+				);
+				const balances = await contract.getAcctInfo(connected);
+				const one = balances.amountNo.toString();
+				const two = balances.amountYes.toString();
+				const three = (balances.avgBuyPriceNo / 1e18).toFixed(2);
+				const four = (balances.avgBuyPriceYes / 1e18).toFixed(2);
+
+				const newObj = {
+					amountNo: one,
+					amountYes: two,
+					avgBuyPriceNo: three,
+					avgBuyPriceYes: four,
+					address: cleanedAllMarkets[index].address,
+					sku: cleanedAllMarkets[index].sku,
+					name: cleanedAllMarkets[index].name,
+				};
+				balanceArray.push(newObj);
+			}
+			setAllBalances(balanceArray);
+		}
+	};
+
+	//------------------ Use Effect / Use memo ------------------
+
+	useEffect(() => {
+		showBalances();
+	}, []);
+
+	return (
+		<div>
+			<Head>
+				<title>Xsauce</title>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+
+			<Layout
+				headerSubtitle={'OVERVIEW'}
+				headerTitle={'Dashboard'}
+				showHowItWorksButton={true}
+				showFinancialOverview={false}
+			>
+				<>
+					<ContentHeader
+						title={'Your Positions'}
+						icon={<img src="/pieChart.svg" />}
+						flexColumn
+					>
+						<div className="flex flex-row items-center mobile:flex-col tablet:space-x-3 mobile:space-y-3  tablet:space-y-0    tablet:flex-row">
+							<text>
+								Total Positions &nbsp;
+								<span className="text-[#748282]">{allBalances?.length}</span>
+							</text>
+						</div>
+					</ContentHeader>
+
+					<DashboardTable>
+						{allBalances.length === 0
+							? skeletonArray.map(() => (
+									<>
+										<Skeleton
+											animation="pulse"
+											variant="rounded"
+											height={70}
+											width={'100%'}
+											sx={{ borderRadius: '100px' }}
+										/>
+										<p className="h-4"> </p>
+									</>
+							  ))
+							: allBalances?.map((el: any) => {
+									return <Dashboard positions={el} />;
+							  })}
+					</DashboardTable>
+				</>
+			</Layout>
+		</div>
+	);
+};
+
+export default Portfolio;
