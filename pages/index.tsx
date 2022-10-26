4878530fca13777c6fc45403db8c8adfc8c7a700
Jessie  Detchou <jessie.detchou@gmail.com>
2022-10-26 11:05:24 -0400

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -127,7 +127,7 @@ const Home: NextPage = () => {
 					</a>
 
 
-					<a className='flex flex-row w-full' href="mailto:someone@example.com">
+					<a className='flex flex-row w-full' href="mailto:info@xsauce.io">
 						<li className='group text-black hover:bg-[#ACFF00] hover:rounded-xl  w-full laptop:w-3/4  px-4 py-2 flex flex-row justify-start items-center'>
 
 							<img className="w-[25px] invisible group-hover:flex mr-4 group-hover:visible" src='/jordans.svg' />
