520f61cb132006413afde432466728041dca2a87
Jessie  Detchou <jessie.detchou@gmail.com>
2022-11-09 08:17:45 -0500

    update dripfeedTopStories

--- components/dripFeed/DripFeedTopStories.jsx
+++ components/dripFeed/DripFeedTopStories.jsx
@@ -34,7 +34,7 @@ export const DripFeedTopStories = () => {
 							alt="topStory#1"
 
 						/>
-						<p className="pt-4 h-[50px]">
+						<p className="pt-2 pl-2 h-[50px]">
 							Hublot Cooks Up A Treat With Damascus Steel Big Bang Unico Gourmet
 						</p>
 					</a>
@@ -48,8 +48,8 @@ export const DripFeedTopStories = () => {
 							className="object-contain w-auto h-[200px] tablet:h-[80%] bg-black "
 							src="sauce.gif"
 						/>
-						<p className="pt-4 h-[50px]">
-							{' '}
+						<p className="pt-2 pl-2 h-[50px]">
+
 							Our thesis on the future of investing in
 							culture
 						</p>
@@ -64,7 +64,7 @@ export const DripFeedTopStories = () => {
 							src="https://www.modern-notoriety.com/wp-content/uploads/2022/09/adidas-yeezy-boost-350-v2-salt-core-black-HQ2060-release-date.jpg"
 							className="object-fit w-auto  h-[200px] tablet:h-[80%] bg-black"
 						/>
-						<p className="pt-4 h-[50px]">
+						<p className="pt-2 pl-2 h-[50px]">
 							Yeezy Boost 350 V2 Dropping in Salt/Core Black
 						</p>
 					</a>
@@ -82,7 +82,7 @@ export const DripFeedTopStories = () => {
 							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 							allowfullscreen
 						></iframe>
-						<p className="pt-4 h-[50px]">Song of the Week</p>
+						<p className="pt-2 pl-2 h-[50px]">Song of the Week</p>
 					</a>
 
 					<a
@@ -94,8 +94,8 @@ export const DripFeedTopStories = () => {
 							className="object-cover w-auto  h-[200px] tablet:h-[80%] bg-black"
 							src="https://static.nike.com/a/images/t_prod/w_1920,c_limit,f_auto,q_auto/243a7c32-0aa7-4c0c-a238-c2b91b91d67e/pdp-replace.jpg"
 						/>
-						<p className="pt-4 h-[50px]">
-							{' '}
+						<p className="pt-2 pl-2 h-[50px]">
+
 							Nike ID is revamped with "Nike by You"
 						</p>
 					</a>
@@ -109,7 +109,7 @@ export const DripFeedTopStories = () => {
 							className="object-contain w-auto  h-[200px] tablet:h-[80%] bg-white"
 							src="https://assets.bigcartel.com/product_images/324756408/0597FD68-35F9-4E9E-ADE6-58B791CCCDF5.jpeg?auto=format&fit=max&w=1200"
 						/>
-						<p className="pt-4 h-[50px]">
+						<p className="pt-2 pl-2 h-[50px]">
 							Atlanta based brand "Currency Crew" is making waves
 						</p>
 					</a>
@@ -123,7 +123,7 @@ export const DripFeedTopStories = () => {
 							src="https://cdn.sanity.io/images/c1chvb1i/production/3984bd83972c6598839d5367ecd17d49866e8dfe-1200x630.jpg"
 							className="w-auto  h-[200px] tablet:h-[80%] bg-black"
 						/>
-						<p className="pt-4 h-[50px]">
+						<p className="pt-2 pl-2 h-[50px]">
 							The greatest nike air force 1s from over the decades
 						</p>
 					</a>
