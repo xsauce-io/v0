786d55a3809b169b443fb6a5dee2d519ea8d3b64
Jessie  Detchou <jessie.detchou@gmail.com>
2022-11-11 21:59:28 -0500

    mint are you lost button

--- components/markets[title]/LostFamButton.jsx
+++ components/markets[title]/LostFamButton.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 export const LostFamButton = () => {
     const [open, setOpen] = useState(false);
+    const ref = useRef(null);
 
+    function handleClickOutside() {
+        setOpen(false);
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+          if (ref.current && !ref.current.contains(event.target)) {
+            setOpen(false);
+          }
+        };
+        document.addEventListener('click', handleClickOutside, true);
+        return () => {
+          document.removeEventListener('click', handleClickOutside, true);
+        };
+      }, []);
 
     return (
-        <div className="px-3">
+        <div className="px-3 " ref={ref} >
             <div className="w-fit px-1 group relative underline font-SG">
                 <button onClick={() => setOpen((previousState)=> !previousState)} className='font-SG hover:opacity-70 underline'>I am lost fam</button>
                 {open ?
