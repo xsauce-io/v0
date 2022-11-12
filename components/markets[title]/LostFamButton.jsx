d45a1c7ae360a3f373918403ef91ad2f6b87a520
Jessie  Detchou <jessie.detchou@gmail.com>
2022-11-11 21:49:37 -0500

    update lost button styling

--- components/markets[title]/LostFamButton.jsx
+++ components/markets[title]/LostFamButton.jsx
@@ -7,8 +7,8 @@ export const LostFamButton = () => {
 
     return (
         <div className="px-3">
-            <div className="w-fit px-1 group relative hover:underline font-SG">
-                <button onClick={() => setOpen((previousState)=> !previousState)} className='font-SG  underline'>I am lost fam</button>
+            <div className="w-fit px-1 group relative underline font-SG">
+                <button onClick={() => setOpen((previousState)=> !previousState)} className='font-SG hover:opacity-70 underline'>I am lost fam</button>
                 {open ?
                     <span className='absolute bg-black bottom-8 right-0 w-[240px] text-sm bg-black p-3 text-white opacity-90 rounded-2xl text-left '>
                         <a target="blank" rel='noreferrer' href="https://discord.com/channels/@me/moonman#8829/" className='text-[#ACFF00]'>Click here</a> to send a message to the Xsauce team for 1 on 1 support.
