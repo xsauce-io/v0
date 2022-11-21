af98654e73e4cc99fb7097989793cff53659c0f2
Jessie  Detchou <jessie.detchou@gmail.com>
2022-11-20 21:02:05 -0500

    change mint to Buy

--- pages/markets/[marketTitle]/index.tsx
+++ pages/markets/[marketTitle]/index.tsx
@@ -132,7 +132,7 @@ const MarketByTitle: NextPage = () => {
 											}
 										>
 
-											Invest
+											Buy
 										</button>
 										<button
 											onClick={() => setTab(1)}
